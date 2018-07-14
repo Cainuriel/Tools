b369c3b42a9d3e3831df2731200cc2f56b829ed2
Unknown <cainuriel@gmail.com>
2018-07-14 13:39:32 +0200

    primeras herramientas String

--- Stringtools/src/stringtools/Stringtools.java
+++ Stringtools/src/stringtools/Stringtools.java
@@ -10,25 +10,40 @@ package stringtools;
  * @author cainu
  */
 public class Stringtools {
-    
-    /** Si se desea recortar un String en base a dos caracteres y no 
-     * a través de una palabra. 
+
+    /**
+     * Si se desea recortar un String en base a dos caracteres y no a través de
+     * una palabra.
+     *
      * @param original cadena a cortar, z caracter final, a caracter inicial.
      * @return cadena recortada
      */
-        public static String cortarStringconchar(String original, String z, String a ) {
+    public static String cortarStringconchar(String original, String z, String a) {
         String substitution;
         String recorte;
-       // el ultimo punto marca el final del nombre, sabiendo que no habra mas.
-      int lastpoint = original.lastIndexOf(z);
-      // como hay mas de un guion bajo sustituimos el primero por un simbolo unico
-      substitution=original.replaceFirst("_","&");
-      // ahora lo buscamos, al simbolo, sabiendo que el ultimo sera el primero al n0 haber mas
-      int firstpoint = substitution.lastIndexOf(a);
-      // se recorta segun esten las posiciones, sumando uno a la primera posicion.
-      recorte = substitution.substring(firstpoint+1, lastpoint);
-        
-        return  recorte;
+        // el ultimo punto marca el final del nombre, sabiendo que no habra mas.
+        int lastpoint = original.lastIndexOf(z);
+        // como hay mas de un guion bajo sustituimos el primero por un simbolo unico
+        substitution = original.replaceFirst("_", "&");
+        // ahora lo buscamos, al simbolo, sabiendo que el ultimo sera el primero al n0 haber mas
+        int firstpoint = substitution.lastIndexOf(a);
+        // se recorta segun esten las posiciones, sumando uno a la primera posicion.
+        recorte = substitution.substring(firstpoint + 1, lastpoint);
+
+        return recorte;
+    }
+
+    //método para calcular el número de veces que se repite un carácter en un String
+    public static int contarCaracteres(String cadena, char caracter) {
+        int posicion, contador = 0;
+        //se busca la primera vez que aparece
+        posicion = cadena.indexOf(caracter);
+        while (posicion != -1) { //mientras se encuentre el caracter
+            contador++;           //se cuenta
+            //se sigue buscando a partir de la posición siguiente a la encontrada
+            posicion = cadena.indexOf(caracter, posicion + 1);
+        }
+        return contador;
     }
 
     /**
@@ -36,15 +51,16 @@ public class Stringtools {
      */
     public static void main(String[] args) {
         // Se saca el nombre del archivo, sabiendo que está entre el primer _ y el último punto. 
-         String actual = "1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION";
-         /**
-          * podemos encontrar el ultimo caracter, pero no el primero de una serie
-          * repetida de caracteres. por ese motivo introducimos un caracter unico,
-          * para cambiarlo por el primero de los caracteres repetidos que nos da el primer corte
-          * el segundo lo marca el ultimo caracter de los puntos, suerte en este caso, por
-          * eso lo introducimos tranquilamente por parametro
-          */
-        System.out.println(cortarStringconchar(actual,".","&"));
+        String actual = "1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION";
+        /**
+         * podemos encontrar el ultimo caracter, pero no el primero de una serie
+         * repetida de caracteres. por ese motivo introducimos un caracter
+         * unico, para cambiarlo por el primero de los caracteres repetidos que
+         * nos da el primer corte el segundo lo marca el ultimo caracter de los
+         * puntos, suerte en este caso, por eso lo introducimos tranquilamente
+         * por parametro
+         */
+        System.out.println(cortarStringconchar(actual, ".", "&"));
     }
-    
+
 }
